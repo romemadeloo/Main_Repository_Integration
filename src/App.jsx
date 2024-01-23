d48b82125419dcb26c2260faa0bcbcd2c6cfbd56
John <unitejohndalev@gmail.com>
2024-01-23 13:51:46 +0800

    intergrated teamb

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+// import Nav from "./components/NavBar/Nav";
+import Nav from '../src/TeamBComponents/components/NavBar/Nav'
+// import Profile from "./components/Profile/Profile";
+import Profile from "../src/TeamBComponents/components/Profile/Profile";
+
+// import Dashboard from "./components/DashBoard/Dashboard";
+import Dashboard from '../src/TeamBComponents/components/DashBoard/Dashboard'
+
+// import CourseList from "./components/CourseList & AddCourseList/CourseList";
+import CourseList from '../src/TeamBComponents/components/CourseList & AddCourseList/CourseList'
+
+// import AddNewCourse from "./components/CourseList & AddCourseList/AddNewCourse";
+import AddNewCourse from "../src/TeamBComponents/components/CourseList & AddCourseList/AddNewCourse";
+
+// import CourseOverview from "./components/CourseList & AddCourseList/CourseOverview";
+import CourseOverview from "./TeamBComponents/components/CourseList & AddCourseList/CourseOverview";
+
+// import EditTopicPage from "./components/Topic/EditTopicPage";
+import EditTopicPage from "./TeamBComponents/components/Topic/EditTopicPage";
+
+// import DisplayTopic from "./components/Topic/DisplayTopic";
+import DisplayTopic from "./TeamBComponents/components/Topic/DisplayTopic";
+
+// import AddTopicTitlePage from "./components/Topic/AddTopicTitlePage";
+import AddTopicTitlePage from "./TeamBComponents/components/Topic/AddTopicTitlePage";
+
+// import EditChapterTitle from "./components/CourseList & AddCourseList/EditChapterTitle";
+import EditChapterTitle from "./TeamBComponents/components/CourseList & AddCourseList/EditChapterTitle";
+
+// import CreateNewChapterTitle from "./components/CourseList & AddCourseList/CreateNewChapterTitle";
+import CreateNewChapterTitle from "./TeamBComponents/components/CourseList & AddCourseList/CreateNewChapterTitle";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-[#EBFFE5]">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/courselist" element={<CourseList />} />
+        <Route path="/addnewcourse" element={<AddNewCourse />} />
+        <Route path="/courseoverview" element={<CourseOverview />} />
+        <Route path="/editchaptertitle" element={<EditChapterTitle />} />
+        <Route
+          path="/createnewchaptertitle"
+          element={<CreateNewChapterTitle />}
+        />
+        <Route path="/addtopictitlepage" element={<AddTopicTitlePage />} />
+        <Route path="/displaytopic" element={<DisplayTopic />} />
+        <Route path="/edittopic" element={<EditTopicPage />} />
+      </Routes>
+
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
