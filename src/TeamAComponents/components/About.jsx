9a489409b51cd6bce64ca924a7ce88694cef7350
jhakes05 <jake.atillo.b@bulsu.edu.ph>
2024-02-15 07:15:10 +0800

    modified about us again with approval of Team A

--- src/TeamAComponents/components/About.jsx
+++ src/TeamAComponents/components/About.jsx
@@ -5,7 +5,10 @@ import React from "react";
 import "../styles/Auth.css";
 import { Link } from "react-router-dom";
 import "../styles/Auth.css";
-
+import pagong from "../../assets/pagong.gif";
+import daga from "../../assets/daga.gif";
+import card1 from "../../assets/card 1.svg";
+import card2 from "../../assets/card 2.svg";
 const About = () => {
   return (
     <>
@@ -35,57 +38,71 @@ const About = () => {
       </nav>
       {/* Main Content Section */}
       {/* className="home-content" old css */}
-
-      <div className="flex flex-row">
-        {/* className="about-container" old css */}
+     <div className="flex"> 
+              <marquee behavior="scroll" direction="right" scrollamount="11" className="absolute">
+                <img src={pagong} alt="" width={30} height={10} />
+              </marquee>
+              <marquee behavior="scroll" direction="right" scrollamount="12" className="absolute">
+                <img src={pagong} alt="" width={30} height={10} />
+              </marquee>
+              <marquee behavior="scroll" direction="right" scrollamount="13" className="absolute">
+                <img src={pagong} alt="" width={30} height={10} />
+              </marquee>
+              <marquee behavior="scroll" direction="right" scrollamount="14" className="absolute">
+                <img src={pagong} alt="" width={30} height={10} />
+              </marquee>
+              <marquee behavior="scroll" direction="right" scrollamount="15" className="absolute">
+                <img src={daga} alt="" width={80} height={10} />
+              </marquee>
+            </div>
+      <div className="flex flex-col TeamB_text-shadow">
         <div className="flex flex-col">
-          <section className="pl-[2rem] pr-[2rem] pt-[1rem]">
-            <h1 className="mt-[15px] text-center font-bold text-[1.5rem]">
-              About Us{" "}
+          <section className="pr-[6rem] pl-[6rem] pt-[2rem] pb-[2rem] p leading-6">
+            {/* className="about-container" old css */}
+            <h1 className="mt-[15px] text-center font-bold text-[2rem]">
+              About Tsukiden{" "}
             </h1>
-            <p className="p-[.5rem] justify-center">
-              Tsukiden Global Solutions, Inc.<b>YOUR RELIABLE IT PARTNER</b>{" "}
-              That's who <b>WE</b> are. For more than 30 years now, Team
+            <p className=" text-center">
+              Tsukiden Global Solutions, Inc.<i>YOUR RELIABLE IT PARTNER </i>
+              That's who <i>WE</i> are. For more than 30 years now, Team
               Tsukiden has always been the trusted reliable IT partner of its
               clients for various projects in Japan, Philippines, Malaysia, Hong
               Kong, Singapore, Taiwan, United Kingdom, and the United States of
-              America.
-            </p>
-            <p className="p-[.5rem] justify-center">
-              Pioneer in IT Outsourcing Tsukiden Global Solutions Inc. is one of
-              the pioneers in IT Outsourcing for the Japanese market here in the
-              Philippines. Since 1989, even before the word “outsourcing” was
-              coined, WE have been sending engineers to Japan for software
-              development. With our years of experience and still growing, there
-              is no other more stable company to outsource to than Tsukiden
-              Global Solutions Inc.
-            </p>
-            <p className="p-[.5rem] justify-center">
-              Tsukiden Global Solutions Inc. provides its clients the advantage
-              of having hardworking, committed and talented manpower on demand,
-              where and when they are needed at a cost that fits the development
-              budget.
+              America. Pioneer in IT Outsourcing Tsukiden Global Solutions Inc.
+              is one of the pioneers in IT Outsourcing for the Japanese market
+              here in the Philippines. Since 1989, even before the word
+              “outsourcing” was coined, WE have been sending engineers to Japan
+              for software development. With our years of experience and still
+              growing, there is no other more stable company to outsource to
+              than Tsukiden Global Solutions Inc. Tsukiden Global Solutions Inc.
+              provides its clients the advantage of having hardworking,
+              committed and talented manpower on demand, where and when they are
+              needed at a cost that fits the development budget.
             </p>
             {/* Include the PhotoSection component here */}
-            {/* className="about-mv" old css classname*/}
           </section>
         </div>
-        <div className="flex justify-end">
-          <section className="flex flex-col pt-[2rem] pr-[2rem]">
-            <div>
-              <h1 className="text-center font-bold text-[1.5rem]">Mission</h1>
+        {/* className="about-mv" old css classname*/}
+        <div>
+          <section className=" flex flex-row leading-6">
+            <div className="border w-[50%] text-center pl-[6rem] pr-[1rem]">
+              <h1 className=" font-bold text-[2rem] ">Mission</h1>
               We aim to... provide excellent quality service to our{" "}
-              <b>CLIENTS </b>, foster professional growth and care to our{" "}
-              <b>EMPLOYEES</b> & develop innovative solutions dedicated for the
-              welfare of our <b>COMMUNITY & SOCIETY.</b>
+              <i>CLIENTS </i>, foster professional growth and care to our{" "}
+              <i>EMPLOYEES</i> & develop innovative solutions dedicated for the
+              welfare of our <i>COMMUNITY & SOCIETY.</i>
+              <div className="pt-[1.5rem]">
+              <img src={card1} alt="" width={500}/>
+              </div>
             </div>
-            <div>
-              <h1 className="text-center pt-[1rem] font-bold text-[1.5rem]">
-                Vision
-              </h1>
-              <div className="justify-center">
-                <b>TGSI</b> is committed to be{" "}
-                <b>THE PREMIER GLOBAL I.T. BUSINESS PARTNER </b>
+            <div className="border w-[50%] pr-[6rem] pl-[1rem]">
+            <div className="pb-[1.5rem] ">
+            <img src={card2} alt="" width={500}/>
+            </div>
+              <h1 className="text-center font-bold text-[2rem] ">Vision</h1>
+              <div className="text-center">
+                <i>TGSI</i> is committed to be{" "}
+                <i>THE PREMIER GLOBAL I.T. BUSINESS PARTNER </i>
                 driven by passion for innovation
               </div>
             </div>
