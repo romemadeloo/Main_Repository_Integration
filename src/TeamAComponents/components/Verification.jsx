d7e31416ea5b1ada8a39335a2ef089707019232b
jdote <154387653+jdote@users.noreply.github.com>
2024-02-17 08:52:38 +0800

    Update Verification.jsx
    Add comments.

--- src/TeamAComponents/components/Verification.jsx
+++ src/TeamAComponents/components/Verification.jsx
@@ -8,21 +8,28 @@ function Verification({ onVerification }) { // Corrected function name
   return (
     <div className="verification-container">
       <div className="verification-header">
+         {/* Logo */}
         <img
           src="../../assets/TeamAassets/companyLogo.png"
           alt="Logo"
           className="verification-logo"
         />
       </div>
+      {/* Container for verification navigation */}
       <div className="verification-navi">
+         {/* Home button */}
         <div className="home-button">
+           {/* Link to home */}
           <Link to="/">
+            {/* Home button */}
             <button>Home</button>
           </Link>
         </div>
       </div>
       <div className="content">
+         {/* Verification sign */}
         <div className="verification-sign">
+          {/* Verification form component */}
           <VerificationForm onVerification={onVerification} />{" "}
           {/* Corrected component name */}
         </div>
