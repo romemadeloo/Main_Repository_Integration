35fe7687bc2b7f5af2ed476b537fc2264c10a2ea
JE <flash.staffsuport@gmail.com>
2024-02-17 10:32:41 +0800

    TEAM C FIX DASHBOARD - JE

--- src/TeamCComponents/pages/course.jsx
+++ src/TeamCComponents/pages/course.jsx
@@ -29,52 +29,12 @@ function TeamC_ChapterSvn() {
             <Link to="/chapters_sql" className="h4 text-white text-decoration-none c_chapter_cardtext">
               {/* Chapter 1 Card Body */}
               <div className="card-body d-flex" id="c_course_cardbody" style={{ backgroundColor: '#126912', borderRadius: '1rem', }}>
-                The SQL Query
-              </div>
-            </Link>
-          </div>
-
-        </div>
-
-        {/* Chapter 2 */}
-        <div className="d-flex align-items-center" id="c_course_itemcontainer">
-
-          {/* Chapter 2 Tag */}
-          <div className='d-flex align-items-center justify-content-center' id="c_course_tag">
-            <span id="c_course_tagtext">BSVN-01</span>
-          </div>
-
-          {/* Chapter 2 Card */}
-          <div className="c_course_cardmain card flex-grow-1" style={{ marginTop: '10px', willChange: 'filter', transition: 'filter 300ms', backgroundColor: '#126912', borderRadius: '1rem', }}>
-            {/* Chapter 2 Link */}
-            <Link to="/chapters_svn" className="h4 text-white text-decoration-none c_chapter_cardtext">
-              {/* Chapter 2 Card Body */}
-              <div className="card-body d-flex" id="c_course_cardbody" style={{ backgroundColor: '#126912', borderRadium: '1rem', }}>
-                Version Control: Subversion
+                {chaptter}
               </div>
             </Link>
           </div>
         </div>
-
-        {/* Chapter 3 */}
-        <div className="d-flex align-items-center" id="c_course_itemcontainer">
-
-          {/* Chapter 3 Tag */}
-          <div className='d-flex align-items-center justify-content-center' id="c_course_tag">
-            <span id="c_course_tagtext">BHPROG-01</span>
-          </div>
-
-          {/* Chapter 3 Card */}
-          <div className="c_course_cardmain card flex-grow-1" style={{ marginTop: '10px', willChange: 'filter', transition: 'filter 300ms', backgroundColor: '#126912', borderRadius: '1rem', }}>
-            {/* Chapter 3 Link */}
-            <Link to="/chapters_hprog" className="h4 text-white text-decoration-none c_chapter_cardtext">
-              {/* Chapter 3 Card Body */}
-              <div className="card-body d-flex" id="c_course_cardbody" style={{ backgroundColor: '#126912', borderRadius: '1rem', }}>
-                HTML Programming Language
-              </div>
-            </Link>
-          </div>
-        </div>
+        
       </div>
     </>
   )
