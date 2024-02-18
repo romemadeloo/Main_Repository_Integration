0e276eb06a49a434d7f383b4a75fb2a7b7ec5d0c
jhakes05 <jake.atillo.b@bulsu.edu.ph>
2024-02-19 07:13:33 +0800

    re-added them for function compatibility across several browsers please do npm i if encountered errors 2/19/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import TeamIntegrationRoutes from "./TeamIntegrationRoutes";
 import TeamC_Router from "./TeamC_Router";
-//added them for function compatibility across several browsers
-
+//re-added them for function compatibility across several browsers please do npm i if encountered errors 2/19/24
+import 'core-js';//Includes polyfills for ECMAScript up to 2023: promises, symbols, collections, iterators, typed arrays, many other features.
+import 'regenerator-runtime/runtime';//runtime support for compiled/transpiled async functions. (It may well have other uses, but this is the predominant one.)
 const App = () => {
   return (
     <>
