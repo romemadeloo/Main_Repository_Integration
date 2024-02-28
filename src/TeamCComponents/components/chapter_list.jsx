ce07920df28b96870ce9989bf9ea6a139f370b32
mellowblu <rrafaelluigirayo@gmail.com>
2024-02-28 13:59:59 +0800

    Routing, adding of components, editing of CSS, coy of quiz, showing of sidebar

--- src/TeamCComponents/components/chapter_list.jsx
+++ src/TeamCComponents/components/chapter_list.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { IoArrowBackCircleSharp } from "react-icons/io5";
 import axios from "axios";
 
 function ChapterList() {
@@ -11,7 +12,7 @@ function ChapterList() {
     useEffect(() => {
         const fetchChapters = async () => {
             try {
-              const result = await axios.get(`http://localhost:8080/api/v1/auth/byCourse/${id}`);
+              const result = await axios.get(`http://localhost:8080/api/courses/${id}`);
         
               // Ensure that result.data is always an array by converting it
               const coursesArray = Array.isArray(result.data)
@@ -33,10 +34,10 @@ console.log(chapters)
             <Link to='/course' className="buttonReturn d-flex align-items-center c_chapter_returncontainer" style={{ textDecoration: 'none', color: 'black', width: 'fit-content', }}>
                 <div className="d-flex align-items-center" style={{ marginTop: '1rem' }}>
                     <div>
-                        <img src="../../src/assets/TeamCassets/green_button.png" className="btnReturn c_chapter_return" alt="return-icon" style={{
-                            transition: 'transform .1s', color: '#ffffff',
+                       <IoArrowBackCircleSharp className="btnReturn c_chapter_return" alt="return-icon" style={{
+                            transition: 'transform .1s', color: '#165207',
                             marginLeft: '1rem', width: '2rem', height: '2rem',
-                        }} />
+                        }}/>
                     </div>
                     <span className="returnTitle c_chapter_returnText" style={{ marginLeft: '0.5rem', color: '#126912', fontSize: '1.5rem', marginTop: '0rem', }}>Back</span>
                 </div>
