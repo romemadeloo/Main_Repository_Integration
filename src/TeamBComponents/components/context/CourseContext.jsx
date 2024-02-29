a1923b68f19a977ea244ef125dbdb91d60d8cee5
judesmcx <lacarjudyann@gmail.com>
2024-02-29 11:30:32 +0800

    added comments

--- src/TeamBComponents/components/context/CourseContext.jsx
+++ src/TeamBComponents/components/context/CourseContext.jsx
@@ -1,38 +1,39 @@
-/* eslint-disable react/prop-types */
-import axios from 'axios';
-import React, { createContext,  useState } from 'react'
+/* eslint-disable react/prop-types */ // Disable eslint warnings for prop-types in this file
+import axios from 'axios'; // Import axios for HTTP requests
+import React, { createContext,  useState } from 'react' // Import React, createContext, and useState from 'react'
 
 
-export const CourseContext = createContext();
+export const CourseContext = createContext(); // Create a context named CourseContext
 
-const CourseProvider = ({children}) => {
+const CourseProvider = ({children}) => { // Define a functional component named CourseProvider receiving children as props 
   //react state for course api
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState([]); // Define state variable courses and setCourses function using useState hook with an initial value of an empty array
 
   //chapter state
-  const [chapters, setChapters] = useState([])
+  const [chapters, setChapters] = useState([]) // Define state variable chapters and setChapters function using useState hook with an initial value of an empty array
 
   //topic state
   // const [topics, setTopics] = useState([])
 
   //hide and show create new course
-  const [showCreateCourse, setShowCreateCourse] = useState(false);
+  const [showCreateCourse, setShowCreateCourse] = useState(false); // Define state variable showCreateCourse and setShowCreateCourse function using useState hook with an initial value of false
 
   
   return (
-    <CourseContext.Provider
+    <CourseContext.Provider // Provide the context values to the components below
       value={{
-        courses,
-        setCourses,
-        showCreateCourse,
-        setShowCreateCourse,
-        chapters,
-        setChapters,
+        courses, // Provide courses state variable
+        setCourses, // Provide setCourses function
+        showCreateCourse, // Provide showCreateCourse state variable
+        setShowCreateCourse, // Provide setShowCreateCourse function
+        chapters, // Provide chapters state variable
+        setChapters, // Provide setChapters function
      
       }}>
-      {children}
-    </CourseContext.Provider>
+      {children} {/* Render the child components */}
+    </CourseContext.Provider> // End of CourseContext.Provider
   );
 }
 
-export default CourseProvider
+export default CourseProvider //Exporting CourseProvider Component 
+//comments by: Judes 02-29-24
