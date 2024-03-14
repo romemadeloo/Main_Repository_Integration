a1d4a20a840e0b88003c7e9f824f6d53281a9e30
rome <romemadelo@gmail.com>
2024-03-14 14:23:56 +0800

    examination folder

---
+++ src/TeamCComponents/pages/examination/instructor/i_examPage.jsx
@@ -0,0 +1,14 @@
+import ContentArea from "./components/examContent";
+
+export default function sExamPage() {
+    return (
+        <div id="parentStudentPage">
+            <div id="childStudentsPage">
+                <ContentArea />
+            </div>
+        </div>
+    );
+}
+
+// NASA index.css ANG BACKGROUND COLOR NG BUONG PAGE
+
